588937fab2c8de64feded81b5746807b1864363e
Angel Romero Herrera <romero@Angels-MacBook-Pro.local>
2018-07-21 23:50:58 -0500

    implemented tasks input, list and localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface status {
+
+}
+
+interface task {
+  title: string;
+  status: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit{
+  static _NEW = 0;
+  static _DONE =  1;
+  private newTask: string;
+  public tasks: task[] ;
+
+  ngOnInit() {
+    this.tasks = JSON.parse(window.localStorage.getItem('tasks')) || [];
+  }
+
+  submit() {
+    this.tasks.push({title: this.newTask, status: AppComponent._NEW})
+    this.newTask = '';
+    window.localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  finishTask() {
+    
+  }
 }
